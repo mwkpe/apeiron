02a62d6bd837e5a57ed445993eda73e1c5c06d48
peter <xe@kthx.de>
2018-03-21 00:25:21 +0100

    Add detail and color option

--- options.h
+++ options.h
@@ -2,6 +2,9 @@
 #define APEIRON_OPTIONS_H
 
 
+#include "color.h"
+
+
 namespace apeiron {
 
 
@@ -13,9 +16,11 @@ struct Options
   bool wireframe = true;
   bool strobe = false;
   bool limit_fps = true;
+  int cylinder_points = 8;
   int max_fps = 120;
   float velocity = 2.0f;
   float sensitivity = 0.02f;
+  Color main_color = Color{0.2f, 0.905f, 0.968f, 1.0f};
 };
 
 
