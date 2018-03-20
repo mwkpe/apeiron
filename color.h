b7a9bd6acc2072c6d829c3dc590c5e4afc681025
peter <xe@kthx.de>
2018-03-21 00:24:04 +0100

    Initial commit

---
+++ color.h
@@ -0,0 +1,20 @@
+#ifndef APEIRON_COLOR_H
+#define APEIRON_COLOR_H
+
+
+namespace apeiron {
+
+
+struct Color
+{
+  float r;
+  float g;
+  float b;
+  float a;
+};
+
+
+}  // namespace apeiron
+
+
+#endif  // APEIRON_COLOR_H
