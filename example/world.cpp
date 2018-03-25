1915d9a327cd5c4a91b089122533f777d81c2b78
peter <xe@kthx.de>
2018-03-25 22:58:57 +0200

    Add ground, cylinder and cubes to scene

--- example/world.cpp
+++ example/world.cpp
@@ -1,6 +1,7 @@
 #include "world.h"
 
 
+#include <random>
 #include <glm/gtc/matrix_transform.hpp>
 
 
@@ -9,6 +10,25 @@ void apeiron::example::World::init()
   renderer_.init();
   car_.load_model("assets/camaro.obj");
   pirate_ship_.load_model("assets/pirate_ship.obj");
+
+  ground_.set_scale(3.6f, 0.1f, 100.0f);
+  ground_.set_position(0.0f, -0.5f, 0.0f);
+  cylinder_.set_position(0.0f, 1.0f, -10.0f);
+
+  std::mt19937 rng;
+  rng.seed(std::random_device{}());
+  std::uniform_int_distribution<std::mt19937::result_type> dist(0, 10000);
+  auto rotation = [&rng, &dist]{ return static_cast<float>(dist(rng)) / 5000.0f - 1.0f; };
+  auto position = [&rng, &dist]{ return static_cast<float>(dist(rng)) / 100.0f - 50.0f; };
+
+  for (int i=0; i<200; ++i) {
+    switch (i % 3) {
+      case 0: poneglyphs_.emplace_back(&cube_, 0.0f, rotation(), rotation()); break;
+      case 1: poneglyphs_.emplace_back(&cube_, rotation(), 0.0f, rotation()); break;
+      case 2: poneglyphs_.emplace_back(&cube_, rotation(), rotation(), 0.0f); break;
+    }
+    poneglyphs_.back().set_position(position(), position(), position());
+  }
 }
 
 
@@ -18,8 +38,11 @@ void apeiron::example::World::reset()
 }
 
 
-void apeiron::example::World::update(float delta_time, const engine::Input* input)
+void apeiron::example::World::update(float time, float delta_time, const engine::Input* input)
 {
+  if (options_->autorotate)
+    frame_time_ = time;
+
   if (input) {
     using Direction = engine::Camera::Direction;
     auto distance = options_->velocity * delta_time;
@@ -34,14 +57,23 @@ void apeiron::example::World::update(float delta_time, const engine::Input* inpu
 
     camera_.orient(input->mouse_x_rel, input->mouse_y_rel, options_->sensitivity);
   }
+
+  cylinder_.set_rotation(frame_time_ * glm::radians(360.0f * options_->cylinder_revs) *
+      cylinder_.rotation_magnitudes());
+  if (cylinder_.points() != options_->cylinder_points) {
+    cylinder_.rebuild(options_->cylinder_points);
+  }
+
+  for (auto& p : poneglyphs_) {
+    p.set_rotation(frame_time_ * glm::radians(120.0f) * p.rotation_magnitudes());
+  }
 }
 
 
 void apeiron::example::World::render(float time)
 {
   frame_++;
-  if (options_->autorotate)
-    frame_time_ = time;
+  auto color = options_->main_color;
 
   renderer_.set_projection(glm::perspective(glm::radians(45.0f), 1280.0f / 720.0f, 0.1f, 1000.0f));
   renderer_.set_view(camera_.view());
@@ -53,8 +85,18 @@ void apeiron::example::World::render(float time)
   else if (options_->strobe)
     renderer_.set_wireframe(false);
 
-  if (options_->pirate_mode)
-    renderer_.render(pirate_ship_, options_->main_color);
-  else
-    renderer_.render(car_, options_->main_color);
+  if (options_->pirate_mode) {
+    renderer_.render(pirate_ship_, color);
+    for (const auto& p : poneglyphs_) {
+      renderer_.render(p, color);
+    }
+  }
+  else {
+    renderer_.render(car_, color);
+  }
+
+  renderer_.render(cylinder_, color);
+
+  renderer_.set_wireframe(false);
+  renderer_.render(ground_, engine::Color{0.2f, 0.2f, 0.2f, 1.0f});
 }
--- example/world.h
+++ example/world.h
@@ -3,11 +3,15 @@
 
 
 #include <cstdint>
+#include <vector>
 #include "options.h"
 #include "engine/input.h"
 #include "engine/camera.h"
+#include "opengl/cube.h"
 #include "opengl/renderer.h"
 #include "example/vehicle.h"
+#include "example/cylinder.h"
+#include "example/poneglyph.h"
 
 
 namespace apeiron::example {
@@ -16,18 +20,23 @@ namespace apeiron::example {
 class World final
 {
 public:
-  World(const Options* options) : options_{options} {}
+  World(const Options* options) : options_{options}, ground_{&cube_},
+      cylinder_{options_->cylinder_points, 0.0f, 0.0f, 1.0f} {}
   void init();
   void reset();
-  void update(float delta_time, const engine::Input* input = nullptr);
+  void update(float time, float delta_time, const engine::Input* input = nullptr);
   void render(float time);
 
 private:
   const Options* options_;
   opengl::Renderer renderer_;
+  opengl::Cube cube_;
   engine::Camera camera_;
+  engine::Entity ground_;
   Vehicle car_;
   Vehicle pirate_ship_;
+  Cylinder cylinder_;
+  std::vector<Poneglyph> poneglyphs_;
   int frame_ = 0;
   float frame_time_ = 0;
 };
