677045702800e613e08eebb1cd03671b2106079f
peter <xe@kthx.de>
2018-03-25 23:02:38 +0200

    Make model a shape

--- opengl/model.h
+++ opengl/model.h
@@ -4,29 +4,17 @@
 
 #include <cstdint>
 #include <string_view>
+#include "opengl/shape.h"
 
 
 namespace apeiron::opengl {
 
 
-class Model final
+class Model final : public Shape
 {
 public:
-  Model() = default;
-  ~Model();
-  // OpenGL delete calls in destructor
-  Model(const Model&) = delete;
-  Model(Model&&) = delete;
-  Model& operator=(const Model&) = delete;
-  Model& operator=(Model&&) = delete;
-
   void load(std::string_view filename);
-  void render() const;
-
-private:
-  std::uint32_t vao_ = 0;
-  std::uint32_t vbo_ = 0;
-  std::uint32_t vertex_count_ = 0;
+  void render() const override;
 };
 
 
