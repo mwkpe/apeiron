1d4397df6277a4712c5545f4e90db82185e1663c
peter <xe@kthx.de>
2018-03-26 19:05:24 +0200

    Change style

--- example/menu.cpp
+++ example/menu.cpp
@@ -12,9 +12,11 @@ namespace {
 void set_style(ImGuiStyle& style)
 {
   style.WindowPadding = ImVec2{8, 8};
-  style.WindowRounding = 5.0f;
+  style.WindowRounding = 0.0f;
+  style.WindowBorderSize = 0.0f;
   style.FramePadding = ImVec2{3, 3};
   style.FrameRounding = 0.0f;
+  style.FrameBorderSize = 0.0f;
   style.ItemSpacing = ImVec2{12, 8};
   style.ItemInnerSpacing = ImVec2{8, 6};
   style.IndentSpacing = 25.0f;
@@ -24,33 +26,26 @@ void set_style(ImGuiStyle& style)
   style.GrabRounding = 5.0f;
 
   ImVec4 almostblack{0.03f, 0.03f, 0.03f, 1.0f};
-  ImVec4 blackglass{0.1f, 0.1f, 0.1f, 0.75f};
-  ImVec4 fuchsia{0.9f, 0.0f, 0.9f, 1.0f};
-  ImVec4 orange{0.996, 0.647, 0.0f, 1.0f};
-  ImVec4 turquoise{0.2f, 0.905f, 0.968f, 1.0f};
-  ImVec4 darkgrey{0.2f, 0.2f, 0.2f, 0.8f};
-  ImVec4 grey{0.25f, 0.25f, 0.25f, 0.8f};
-  ImVec4 lightgrey{0.4f, 0.4f, 0.4f, 0.8f};
+  ImVec4 blackglass{0.05f, 0.05f, 0.05f, 0.9f};
+  ImVec4 darkgrey{0.15f, 0.15f, 0.15f, 0.9f};
+  ImVec4 grey{0.2f, 0.2f, 0.2f, 0.9f};
+  ImVec4 lightgrey{0.4f, 0.4f, 0.4f, 0.9f};
 
   style.Colors[ImGuiCol_WindowBg] = blackglass;
-  style.Colors[ImGuiCol_Border] = orange;
   style.Colors[ImGuiCol_FrameBg] = darkgrey;
   style.Colors[ImGuiCol_FrameBgHovered] = grey;
   style.Colors[ImGuiCol_FrameBgActive] = grey;
-  style.Colors[ImGuiCol_Text] = orange;
   style.Colors[ImGuiCol_TextDisabled] = grey;
-  style.Colors[ImGuiCol_TitleBg] = blackglass;
-  style.Colors[ImGuiCol_TitleBgCollapsed] = blackglass;
+  style.Colors[ImGuiCol_TitleBg] = darkgrey;
+  style.Colors[ImGuiCol_TitleBgCollapsed] = darkgrey;
   style.Colors[ImGuiCol_TitleBgActive] = grey;
-  style.Colors[ImGuiCol_ResizeGrip] = almostblack;
+  style.Colors[ImGuiCol_ResizeGrip] = darkgrey;
   style.Colors[ImGuiCol_ResizeGripHovered] = lightgrey;
-  style.Colors[ImGuiCol_ResizeGripActive] = orange;
   style.Colors[ImGuiCol_SliderGrab] = almostblack;
   style.Colors[ImGuiCol_SliderGrabActive] = lightgrey;
   style.Colors[ImGuiCol_Button] = darkgrey;
   style.Colors[ImGuiCol_ButtonHovered] = grey;
   style.Colors[ImGuiCol_ButtonActive] = lightgrey;
-  style.Colors[ImGuiCol_CheckMark] = orange;
 }
 
 
@@ -92,7 +87,6 @@ void apeiron::example::Menu::build(example::Options* options, float time)
     ImGui::SliderFloat("Cyliner rev/s", &options->cylinder_revs, 0.1f, 1.0f);
     ImGui::Checkbox("Autorotate", &options->autorotate);
     ImGui::Checkbox("Wireframe (F2)", &options->wireframe);
-    ImGui::Checkbox("Strobe", &options->strobe);
     ImGui::Checkbox("Arr!", &options->pirate_mode);
     ImGui::Text("Camera");
     ImGui::SliderFloat("Velocity (m/s)", &options->velocity, 0.0f, 20.0f);
