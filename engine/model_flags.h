f6d184d857d4f2b11026ef055e4371fc51e21e9c
peter <xe@kthx.de>
2018-04-01 03:08:35 +0200

    Initial commit

---
+++ engine/model_flags.h
@@ -0,0 +1,14 @@
+#ifndef APEIRON_ENGINE_MODEL_FLAGS_H
+#define APEIRON_ENGINE_MODEL_FLAGS_H
+
+
+namespace apeiron::engine::model_flags {
+
+
+enum { vertices = 0x1, normals = 0x2, tex_coords = 0x4, colors = 0x8 };
+
+
+}  // namespace apeiron::engine::model_flags
+
+
+#endif  // APEIRON_ENGINE_MODEL_FLAGS_H
