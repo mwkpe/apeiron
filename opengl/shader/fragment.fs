a07467eef1b9f4782c590bf74ac654acddf6ad98
peter <xe@kthx.de>
2018-04-01 03:12:47 +0200

    Implement color mode switch

--- opengl/shader/fragment.fs
+++ opengl/shader/fragment.fs
@@ -1,18 +1,27 @@
 #version 330 core
 
-uniform bool draw_texture;
+uniform int color_mode;
 uniform sampler2D texture2d;
 uniform vec4 color;
 
 out vec4 frag_color;
+in vec3 normal;
 in vec2 tex_coord;
+in vec3 vertex_color;
 
 void main()
 {
-  if (draw_texture) {
-    frag_color = texture(texture2d, tex_coord);
-  }
-  else {
-    frag_color = color;
+  switch (color_mode) {
+    case 0:
+      frag_color = texture(texture2d, tex_coord);
+      break;
+    case 1:
+      frag_color = vec4(vertex_color, 1.0);
+      break;
+    case 2:
+      frag_color = color;
+      break;
+    default:
+      frag_color = vec4(1.0, 0.0, 1.0, 1.0);
   }
 }
--- opengl/shader/vertex.vs
+++ opengl/shader/vertex.vs
@@ -1,16 +1,22 @@
 #version 330 core
 
 layout (location = 0) in vec3 a_position;
-layout (location = 1) in vec2 a_tex_coord;
+layout (location = 1) in vec3 a_normal;
+layout (location = 2) in vec2 a_tex_coord;
+layout (location = 3) in vec3 a_color;
 
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
+out vec3 normal;
 out vec2 tex_coord;
+out vec3 vertex_color;
 
 void main()
 {
   gl_Position = projection * view * model * vec4(a_position, 1.0);
+  normal = a_normal;
   tex_coord = a_tex_coord;
+  vertex_color = a_color;
 }
