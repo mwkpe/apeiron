c6f9c1d6cf1906cd806656fbd8d49b0906422d4a
peter <xe@kthx.de>
2018-04-03 20:20:25 +0200

    Add size and center

--- engine/entity.h
+++ engine/entity.h
@@ -12,22 +12,32 @@ namespace apeiron::engine {
 class Entity
 {
 public:
-  Entity() : scale_{1.0f, 1.0f, 1.0f} {}
+  Entity() = default;
+  explicit Entity(glm::vec3 size) : size_{size} {}
+  void set_size(float x, float y, float z) { size_ = glm::vec3{x, y, z}; };
+  void set_size(glm::vec3 size) { size_ = size; };
+  void set_center(float x, float y, float z) { center_ = glm::vec3{x, y, z}; };
+  void set_center(glm::vec3 center) { center_ = center; };
   void set_position(float x, float y, float z) { position_ = glm::vec3{x, y, z}; };
   void set_position(glm::vec3 position) { position_ = position; };
   void set_rotation(float x, float y, float z) { rotation_ = glm::vec3{x, y, z}; };
   void set_rotation(glm::vec3 rotation) { rotation_ = rotation; };
   void set_scale(float x, float y, float z) { scale_ = glm::vec3{x, y, z}; };
   void set_scale(glm::vec3 scale) { scale_ = scale; };
+  glm::vec3 center() const { return center_; }
+  glm::vec3 size() const { return size_; }
   glm::vec3 position() const { return position_; }
   glm::vec3 rotation() const { return rotation_; }
   glm::vec3 scale() const { return scale_; }
   virtual void render() const {};
+  virtual void render_bounds() const {};
 
 protected:
-  glm::vec3 position_;
-  glm::vec3 rotation_;
-  glm::vec3 scale_;
+  glm::vec3 size_ = glm::vec3{1.0f, 1.0f, 1.0f};
+  glm::vec3 position_ = glm::vec3{0.0f, 0.0f, 0.0f};
+  glm::vec3 rotation_ = glm::vec3{0.0f, 0.0f, 0.0f};
+  glm::vec3 scale_ = glm::vec3{1.0f, 1.0f, 1.0f};
+  glm::vec3 center_ = glm::vec3{0.0f, 0.0f, 0.0f};
 };
 
 
