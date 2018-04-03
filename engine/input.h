010f29c91a600ef119420966312d4aacb1a8dc7d
peter <xe@kthx.de>
2018-04-03 20:19:46 +0200

    Add default values

--- engine/input.h
+++ engine/input.h
@@ -7,14 +7,14 @@ namespace apeiron::engine {
 
 struct Input
 {
-  bool forward;
-  bool backward;
-  bool left;
-  bool right;
-  int mouse_x_abs;
-  int mouse_x_rel;
-  int mouse_y_abs;
-  int mouse_y_rel;
+  bool forward = false;
+  bool backward = false;
+  bool left = false;
+  bool right = false;
+  int mouse_x_abs = 0;
+  int mouse_x_rel = 0;
+  int mouse_y_abs = 0;
+  int mouse_y_rel = 0;
 };
 
 
