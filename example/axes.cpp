0b5901d9490c348d15b278867afae6e0b3f6756d
peter <pemacij@gmail.com>
2018-09-22 00:11:36 +0200

    Remove tabs, wtf

--- example/axes.cpp
+++ example/axes.cpp
@@ -3,20 +3,20 @@
 
 
 apeiron::example::Axes::Axes(int points, float radius, float height)
-		: x_axis_{points, radius, height}, y_axis_{points, radius, height},
-			z_axis_{points, radius, height}
+    : x_axis_{points, radius, height}, y_axis_{points, radius, height},
+      z_axis_{points, radius, height}
 {
-	// Rotate so +z goes outside the screen, -z into the screen
-	z_axis_.set_rotation(glm::radians(-90.0f), 0.0f, 0.0f);
-	// Rotate so -x goes to the left and +x goes to the right
-	x_axis_.set_rotation(0.0f, 0.0f, glm::radians(-90.0f));
-	// Y is up axis, cylinder is already build in this orientation
+  // Rotate so +z goes outside the screen, -z into the screen
+  z_axis_.set_rotation(glm::radians(-90.0f), 0.0f, 0.0f);
+  // Rotate so -x goes to the left and +x goes to the right
+  x_axis_.set_rotation(0.0f, 0.0f, glm::radians(-90.0f));
+  // Y is up axis, cylinder is already build in this orientation
 }
 
 
 void apeiron::example::Axes::render(opengl::Renderer& renderer) const
 {
-	renderer.use_color_shading();
+  renderer.use_color_shading();
   renderer.render(x_axis_, {1.0f, 0.0f, 0.0f, 1.0f});
   renderer.render(y_axis_, {0.0f, 1.0f, 0.0f, 1.0f});
   renderer.render(z_axis_, {0.0f, 0.0f, 1.0f, 1.0f});
