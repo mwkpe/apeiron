6d6d440264db09f4bbc181081523afed2d75997f
peter <pemacij@gmail.com>
2018-09-26 10:12:03 +0200

    Change texture wrap mode

--- opengl/texture.cpp
+++ opengl/texture.cpp
@@ -22,8 +22,8 @@ void apeiron::opengl::Texture::load(std::string_view filename)
     glDeleteTextures(1, &id_);
   glGenTextures(1, &id_);
   glBindTexture(GL_TEXTURE_2D, id_);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
   if (channel_count == 4)
