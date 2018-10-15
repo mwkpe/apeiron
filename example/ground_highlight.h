c20b168e3401fba8541ac594b90557a0dcd36c6c
peter <pemacij@gmail.com>
2018-10-15 20:05:09 +0200

    Initial commit

---
+++ example/ground_highlight.h
@@ -0,0 +1,30 @@
+#ifndef APEIRON_EXAMPLE_GROUND_HIGHLIGHT_H
+#define APEIRON_EXAMPLE_GROUND_HIGHLIGHT_H
+
+
+#include <glm/glm.hpp>
+#include "engine/entity.h"
+#include "opengl/quad.h"
+
+
+namespace apeiron::example {
+
+
+class Ground_highlight final : public engine::Entity
+{
+public:
+  Ground_highlight() : quad_{0.8f, 0.8f} { set_rotation(glm::radians(-90.0f), 0.0f, 0.0f); }
+  void set_visible(bool visible) { visible_ = visible; }
+  bool visible() { return visible_; }
+  void render() const override { quad_.render(); }
+
+private:
+  opengl::Quad quad_;
+  bool visible_ = false;
+};
+
+
+}  // namespace apeiron::example
+
+
+#endif  // APEIRON_EXAMPLE_GROUND_HIGHLIGHT_H
