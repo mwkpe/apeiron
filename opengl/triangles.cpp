b393c65ce519dcee474bdbd7d2d849af0ef56f04
peter <pemacij@gmail.com>
2018-10-31 20:43:42 +0100

    Add overloads

--- opengl/triangles.cpp
+++ opengl/triangles.cpp
@@ -5,6 +5,38 @@
 #include "GL/glew.h"
 
 
+apeiron::opengl::Triangles::Triangles(std::vector<engine::Vertex>&& vertices)
+{
+  vertex_count_ = vertices.size();
+
+  glGenVertexArrays(1, &vao_);
+  glGenBuffers(1, &vbo_);
+  glBindVertexArray(vao_);
+
+  const int stride = sizeof(engine::Vertex);
+
+  glBindBuffer(GL_ARRAY_BUFFER, vbo_);
+  glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(engine::Vertex),
+      vertices.data(), GL_STATIC_DRAW);
+
+  // Position
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, reinterpret_cast<void*>(0));
+  glEnableVertexAttribArray(0);
+  // Normal
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, stride,
+      reinterpret_cast<void*>(offsetof(engine::Vertex, normal)));
+  glEnableVertexAttribArray(1);
+  // Texture coordinates
+  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, stride,
+      reinterpret_cast<void*>(offsetof(engine::Vertex, texcoords)));
+  glEnableVertexAttribArray(2);
+  // Color
+  glVertexAttribPointer(3, 4, GL_FLOAT, GL_FALSE, stride,
+      reinterpret_cast<void*>(offsetof(engine::Vertex, color)));
+  glEnableVertexAttribArray(3);
+}
+
+
 apeiron::opengl::Triangles::Triangles(std::vector<engine::Vertex_simple>&& vertices)
 {
   vertex_count_ = vertices.size();
@@ -20,6 +52,30 @@ apeiron::opengl::Triangles::Triangles(std::vector<engine::Vertex_simple>&& verti
   // Position
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, reinterpret_cast<void*>(0));
   glEnableVertexAttribArray(0);
+}
+
+
+apeiron::opengl::Triangles::Triangles(std::vector<engine::Vertex_normal>&& vertices)
+{
+  vertex_count_ = vertices.size();
+
+  glGenVertexArrays(1, &vao_);
+  glGenBuffers(1, &vbo_);
+  glBindVertexArray(vao_);
+
+  const int stride = sizeof(engine::Vertex_normal);
+
+  glBindBuffer(GL_ARRAY_BUFFER, vbo_);
+  glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(engine::Vertex_normal),
+      vertices.data(), GL_STATIC_DRAW);
+
+  // Position
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, reinterpret_cast<void*>(0));
+  glEnableVertexAttribArray(0);
+  // Normal
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, stride,
+      reinterpret_cast<void*>(offsetof(engine::Vertex_normal, normal)));
+  glEnableVertexAttribArray(1);
 }
 
 
@@ -44,6 +100,86 @@ apeiron::opengl::Triangles::Triangles(std::vector<engine::Vertex_color>&& vertic
   glVertexAttribPointer(3, 4, GL_FLOAT, GL_FALSE, stride,
       reinterpret_cast<void*>(offsetof(engine::Vertex_color, color)));
   glEnableVertexAttribArray(3);
+}
+
+
+apeiron::opengl::Triangles::Triangles(std::vector<engine::Vertex_texcoords>&& vertices)
+{
+  vertex_count_ = vertices.size();
+
+  glGenVertexArrays(1, &vao_);
+  glGenBuffers(1, &vbo_);
+  glBindVertexArray(vao_);
+
+  const int stride = sizeof(engine::Vertex_texcoords);
+
+  glBindBuffer(GL_ARRAY_BUFFER, vbo_);
+  glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(engine::Vertex_texcoords),
+      vertices.data(), GL_STATIC_DRAW);
+
+  // Position
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, reinterpret_cast<void*>(0));
+  glEnableVertexAttribArray(0);
+  // Texture coordinates
+  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, stride,
+      reinterpret_cast<void*>(offsetof(engine::Vertex_texcoords, texcoords)));
+  glEnableVertexAttribArray(2);
+}
+
+
+apeiron::opengl::Triangles::Triangles(std::vector<engine::Vertex_normal_color>&& vertices)
+{
+  vertex_count_ = vertices.size();
+
+  glGenVertexArrays(1, &vao_);
+  glGenBuffers(1, &vbo_);
+  glBindVertexArray(vao_);
+
+  const int stride = sizeof(engine::Vertex_normal_color);
+
+  glBindBuffer(GL_ARRAY_BUFFER, vbo_);
+  glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(engine::Vertex_normal_color),
+      vertices.data(), GL_STATIC_DRAW);
+
+  // Position
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, reinterpret_cast<void*>(0));
+  glEnableVertexAttribArray(0);
+  // Normal
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, stride,
+      reinterpret_cast<void*>(offsetof(engine::Vertex_normal_color, normal)));
+  glEnableVertexAttribArray(1);
+  // Color
+  glVertexAttribPointer(3, 4, GL_FLOAT, GL_FALSE, stride,
+      reinterpret_cast<void*>(offsetof(engine::Vertex_normal_color, color)));
+  glEnableVertexAttribArray(3);
+}
+
+
+apeiron::opengl::Triangles::Triangles(std::vector<engine::Vertex_normal_texcoords>&& vertices)
+{
+  vertex_count_ = vertices.size();
+
+  glGenVertexArrays(1, &vao_);
+  glGenBuffers(1, &vbo_);
+  glBindVertexArray(vao_);
+
+  const int stride = sizeof(engine::Vertex_normal_texcoords);
+
+  glBindBuffer(GL_ARRAY_BUFFER, vbo_);
+  glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(engine::Vertex_normal_texcoords),
+      vertices.data(), GL_STATIC_DRAW);
+
+  // Position
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, reinterpret_cast<void*>(0));
+  glEnableVertexAttribArray(0);
+  // Normal
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, stride,
+      reinterpret_cast<void*>(offsetof(engine::Vertex_normal_texcoords, normal)));
+  glEnableVertexAttribArray(1);
+  // Texture coordinates
+  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, stride,
+      reinterpret_cast<void*>(offsetof(engine::Vertex_normal_texcoords, texcoords)));
+  glEnableVertexAttribArray(2);
 }
 
 
--- opengl/triangles.h
+++ opengl/triangles.h
@@ -14,8 +14,13 @@ namespace apeiron::opengl {
 class Triangles final : public Shape
 {
 public:
+  explicit Triangles(std::vector<engine::Vertex>&& vertices);
   explicit Triangles(std::vector<engine::Vertex_simple>&& vertices);
+  explicit Triangles(std::vector<engine::Vertex_normal>&& vertices);
   explicit Triangles(std::vector<engine::Vertex_color>&& vertices);
+  explicit Triangles(std::vector<engine::Vertex_texcoords>&& vertices);
+  explicit Triangles(std::vector<engine::Vertex_normal_color>&& vertices);
+  explicit Triangles(std::vector<engine::Vertex_normal_texcoords>&& vertices);
   void render() const override;
   void render(std::uint32_t start, std::uint32_t count) const override;
 };
