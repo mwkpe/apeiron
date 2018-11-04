c88a0499d429fd62b23195dc9d89c2aee4b2b3b6
peter <pemacij@gmail.com>
2018-11-04 13:45:54 +0100

    Readd wireframe cuboid

---
+++ opengl/wire_cuboid.h
@@ -0,0 +1,27 @@
+#ifndef APEIRON_OPENGL_WIRE_CUBOID_H
+#define APEIRON_OPENGL_WIRE_CUBOID_H
+
+
+#include <glm/glm.hpp>
+#include "opengl/shape.h"
+
+
+namespace apeiron::opengl {
+
+
+class Wire_cuboid final : public Shape
+{
+public:
+  explicit Wire_cuboid(glm::vec3 size, glm::vec3 position = glm::vec3{0.0f});
+  Wire_cuboid(glm::vec3 size, glm::vec4 color, glm::vec3 position = glm::vec3{0.0f});
+  void render() const override;
+
+private:
+	float line_width_ = 1.0f;
+};
+
+
+}  // namespace apeiron::opengl
+
+
+#endif  // APEIRON_OPENGL_WIRE_CUBOID_H
