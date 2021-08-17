43c159dce93cfc438027cb6f4411023577aa9dda
mwkpe <pemacij@gmail.com>
2021-08-17 21:59:58 +0200

    Restructure assets

--- prefab/teapot.h
+++ prefab/teapot.h
@@ -13,7 +13,7 @@ namespace apeiron::prefab {
 class Teapot final : public engine::Entity
 {
 public:
-  void load_model() { model_.load<engine::Vertex_normal>("assets/utah_teapot.obj"); }
+  void load_model() { model_.load<engine::Vertex_normal>("assets/models/utah_teapot.obj"); }
   void render() const override { for (const auto& mesh : model_) mesh.render(); }
 
 private:
