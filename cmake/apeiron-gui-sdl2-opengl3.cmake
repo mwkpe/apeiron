590fa8d76948d1416ecd96b3f0f2c30ae3696d8e
mwkpe <pemacij@gmail.com>
2022-10-11 11:22:38 +0200

    Fix for MSVC support

--- cmake/apeiron-gui-sdl2-opengl3.cmake
+++ cmake/apeiron-gui-sdl2-opengl3.cmake
@@ -1,3 +1,5 @@
+set(SDL2_DIR "${CMAKE_CURRENT_LIST_DIR}/../external/SDL2")
+
 add_library(apeiron-gui-sdl2-opengl3
     ${CMAKE_CURRENT_LIST_DIR}/../external/imgui/imgui.cpp
     ${CMAKE_CURRENT_LIST_DIR}/../external/imgui/imgui.h
@@ -13,13 +15,18 @@ add_library(apeiron-gui-sdl2-opengl3
     ${CMAKE_CURRENT_LIST_DIR}/../opengl/gui.cpp
     ${CMAKE_CURRENT_LIST_DIR}/../opengl/gui.h)
 
-if (WIN32)
-  include_directories(${CMAKE_CURRENT_SOURCE_DIR}/../external/SDL2/x86_64-w64-mingw32/include)
-  link_directories(${CMAKE_CURRENT_SOURCE_DIR}/../external/SDL2/x86_64-w64-mingw32/lib)
+if (WIN32 AND CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    include_directories(${SDL2_DIR}/x86_64-w64-mingw32/include)
+    link_directories(${SDL2_DIR}/x86_64-w64-mingw32/lib)
+elseif (WIN32 AND CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+    include_directories(${SDL2_DIR}/include)
+    link_directories(${SDL2_DIR}/lib)
 endif ()
 
-if (WIN32)
-  target_link_libraries(apeiron-gui-sdl2-opengl3 glad -limm32 -lmingw32 -lSDL2main -lSDL2)
+if (WIN32 AND CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    target_link_libraries(apeiron-gui-sdl2-opengl3 glad -limm32 -lmingw32 -lSDL2main -lSDL2)
+elseif (WIN32 AND CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+    target_link_libraries(apeiron-gui-sdl2-opengl3 glad imm32 SDL2main SDL2)
 elseif (UNIX AND NOT APPLE)
   target_link_libraries(apeiron-gui-sdl2-opengl3 glad -lSDL2main -lSDL2)
 endif ()
