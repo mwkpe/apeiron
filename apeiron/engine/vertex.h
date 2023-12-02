512c718f63fe62026611f6caf0a0ebacbeac904b
mwkpe <mwkpe@proton.me>
2023-12-02 05:52:40 +0100

    Add const

--- apeiron/engine/vertex.h
+++ apeiron/engine/vertex.h
@@ -11,11 +11,22 @@ namespace apeiron::engine {
 struct Vertex
 {
   Vertex() = default;
-  Vertex(float x, float y, float z, float nx, float ny, float nz,
-      float s, float t, float r, float g, float b, float a = 1.0f)
-      : position{x, y, z}, normal{nx, ny, nz}, texcoords{s, t}, color{r, g, b, a} {}
-  Vertex(glm::vec3 position, glm::vec3 normal, glm::vec2 texcoords, glm::vec4 color)
-      : position{position}, normal{normal}, texcoords{texcoords}, color{color} {}
+  constexpr Vertex(float x, float y, float z,
+      float nx, float ny, float nz,
+      float s, float t,
+      float r, float g, float b, float a = 1.0f)
+      : position{x, y, z},
+        normal{nx, ny, nz},
+        texcoords{s, t},
+        color{r, g, b, a} {}
+  constexpr Vertex(const glm::vec3& position,
+      const glm::vec3& normal,
+      const glm::vec2& texcoords,
+      const glm::vec4& color)
+      : position{position},
+        normal{normal},
+        texcoords{texcoords},
+        color{color} {}
   glm::vec3 position;
   glm::vec3 normal;
   glm::vec2 texcoords;
@@ -27,7 +38,7 @@ struct Vertex_simple
 {
   Vertex_simple() = default;
   Vertex_simple(float x, float y, float z) : position{x, y, z} {}
-  explicit Vertex_simple(glm::vec3 position) : position{position} {}
+  constexpr explicit Vertex_simple(const glm::vec3& position) : position{position} {}
   glm::vec3 position;
 };
 
@@ -35,9 +46,9 @@ struct Vertex_simple
 struct Vertex_normal
 {
   Vertex_normal() = default;
-  Vertex_normal(float x, float y, float z, float nx, float ny, float nz)
+  constexpr Vertex_normal(float x, float y, float z, float nx, float ny, float nz)
       : position{x, y, z}, normal{nx, ny, nz} {}
-  Vertex_normal(glm::vec3 position, glm::vec3 normal)
+  constexpr Vertex_normal(const glm::vec3& position, const glm::vec3& normal)
       : position{position}, normal{normal} {}
   glm::vec3 position;
   glm::vec3 normal;
@@ -47,9 +58,9 @@ struct Vertex_normal
 struct Vertex_color
 {
   Vertex_color() = default;
-  Vertex_color(float x, float y, float z, float r, float g, float b, float a = 1.0f)
+  constexpr Vertex_color(float x, float y, float z, float r, float g, float b, float a = 1.0f)
       : position{x, y, z}, color{r, g, b, a} {}
-  Vertex_color(glm::vec3 position, glm::vec4 color)
+  constexpr Vertex_color(const glm::vec3& position, const glm::vec4& color)
       : position{position}, color{color} {}
   glm::vec3 position;
   glm::vec4 color;
@@ -59,9 +70,9 @@ struct Vertex_color
 struct Vertex_texcoords
 {
   Vertex_texcoords() = default;
-  Vertex_texcoords(float x, float y, float z, float s, float t)
+  constexpr Vertex_texcoords(float x, float y, float z, float s, float t)
       : position{x, y, z}, texcoords{s, t} {}
-  Vertex_texcoords(glm::vec3 position, glm::vec2 texcoords)
+  constexpr Vertex_texcoords(const glm::vec3& position, const glm::vec2& texcoords)
       : position{position}, texcoords{texcoords} {}
   glm::vec3 position;
   glm::vec2 texcoords;
@@ -71,11 +82,18 @@ struct Vertex_texcoords
 struct Vertex_normal_color
 {
   Vertex_normal_color() = default;
-  Vertex_normal_color(float x, float y, float z, float nx, float ny, float nz,
+  constexpr Vertex_normal_color(float x, float y, float z,
+      float nx, float ny, float nz,
       float r, float g, float b, float a = 1.0f)
-      : position{x, y, z}, normal{nx, ny, nz}, color{r, g, b, a} {}
-  Vertex_normal_color(glm::vec3 position, glm::vec3 normal, glm::vec4 color)
-      : position{position}, normal{normal}, color{color} {}
+      : position{x, y, z},
+        normal{nx, ny, nz},
+        color{r, g, b, a} {}
+  constexpr Vertex_normal_color(const glm::vec3& position,
+      const glm::vec3& normal,
+      const glm::vec4& color)
+      : position{position},
+        normal{normal},
+        color{color} {}
   glm::vec3 position;
   glm::vec3 normal;
   glm::vec4 color;
@@ -85,10 +103,18 @@ struct Vertex_normal_color
 struct Vertex_normal_texcoords
 {
   Vertex_normal_texcoords() = default;
-  Vertex_normal_texcoords(float x, float y, float z, float nx, float ny, float nz,
-      float s, float t) : position{x, y, z}, normal{nx, ny, nz}, texcoords{s, t} {}
-  Vertex_normal_texcoords(glm::vec3 position, glm::vec3 normal, glm::vec2 texcoords)
-      : position{position}, normal{normal}, texcoords{texcoords} {}
+  constexpr Vertex_normal_texcoords(float x, float y, float z,
+      float nx, float ny, float nz,
+      float s, float t)
+      : position{x, y, z},
+        normal{nx, ny, nz},
+        texcoords{s, t} {}
+  constexpr Vertex_normal_texcoords(const glm::vec3& position,
+      const glm::vec3& normal,
+      const glm::vec2& texcoords)
+      : position{position},
+        normal{normal},
+        texcoords{texcoords} {}
   glm::vec3 position;
   glm::vec3 normal;
   glm::vec2 texcoords;
