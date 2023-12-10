7de26707887bf4bb117eab485200d7acf2d14912
mwkpe <mwkpe@proton.me>
2023-12-10 14:30:54 +0100

    Change error constructors

--- apeiron/engine/error.h
+++ apeiron/engine/error.h
@@ -15,10 +15,10 @@ class Error : public std::runtime_error
 public:
   explicit Error(const std::string& s) : std::runtime_error{s} {}
   explicit Error(const char* s) : std::runtime_error{s} {}
-  Error(const std::string& s, const std::string& p)
-      : std::runtime_error{s + p} {}
-  Error(const std::string& s, std::string_view p)
-      : std::runtime_error{s + std::string{p}} {}
+  Error(const std::string& s1, const std::string& s2)
+      : std::runtime_error{s1 + ": " + s2} {}
+  Error(std::string_view s1, std::string_view s2)
+      : std::runtime_error{std::string{s1} + ": " + std::string{s2}} {}
 };
 
 
@@ -27,10 +27,10 @@ class Warning : public std::runtime_error
 public:
   explicit Warning(const std::string& s) : std::runtime_error{s} {}
   explicit Warning(const char* s) : std::runtime_error{s} {}
-  Warning(const std::string& s, const std::string& p)
-      : std::runtime_error{s + p} {}
-  Warning(const std::string& s, std::string_view p)
-      : std::runtime_error{s + std::string{p}} {}
+  Warning(const std::string& s1, const std::string& s2)
+      : std::runtime_error{s1 + ": " + s2} {}
+  Warning(std::string_view s1, std::string_view s2)
+      : std::runtime_error{std::string{s1} + ": " + std::string{s2}} {}
 };
 
 
