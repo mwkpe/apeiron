7586a4d03aefb744d8692afb9c51816b81c6b9e3
mwkpe <mwkpe@proton.me>
2024-03-31 00:27:14 +0100

    Add integer conversions

--- apeiron/engine/color_converter.h
+++ apeiron/engine/color_converter.h
@@ -34,12 +34,12 @@ namespace apeiron::engine {
 constexpr glm::vec3 as_rgb(std::string_view hex_code)
 {
   if (hex_code.size() < 7 || hex_code[0] != '#')
-    return {255, 255, 255};
+    return {0xff, 0xff, 0xff};
 
   return {
-    detail::as_decimal(hex_code[1]) << 4 | detail::as_decimal(hex_code[2]),
-    detail::as_decimal(hex_code[3]) << 4 | detail::as_decimal(hex_code[4]),
-    detail::as_decimal(hex_code[5]) << 4 | detail::as_decimal(hex_code[6])
+      detail::as_decimal(hex_code[1]) << 4 | detail::as_decimal(hex_code[2]),
+      detail::as_decimal(hex_code[3]) << 4 | detail::as_decimal(hex_code[4]),
+      detail::as_decimal(hex_code[5]) << 4 | detail::as_decimal(hex_code[6])
   };
 }
 
@@ -47,13 +47,13 @@ constexpr glm::vec3 as_rgb(std::string_view hex_code)
 constexpr glm::vec4 as_rgba(std::string_view hex_code)
 {
   if (hex_code.size() < 9 || hex_code[0] != '#')
-    return {255, 255, 255, 255};
+    return {0xff, 0xff, 0xff, 0xff};
 
   return {
-    detail::as_decimal(hex_code[1]) << 4 | detail::as_decimal(hex_code[2]),
-    detail::as_decimal(hex_code[3]) << 4 | detail::as_decimal(hex_code[4]),
-    detail::as_decimal(hex_code[5]) << 4 | detail::as_decimal(hex_code[6]),
-    detail::as_decimal(hex_code[7]) << 4 | detail::as_decimal(hex_code[8])
+      detail::as_decimal(hex_code[1]) << 4 | detail::as_decimal(hex_code[2]),
+      detail::as_decimal(hex_code[3]) << 4 | detail::as_decimal(hex_code[4]),
+      detail::as_decimal(hex_code[5]) << 4 | detail::as_decimal(hex_code[6]),
+      detail::as_decimal(hex_code[7]) << 4 | detail::as_decimal(hex_code[8])
   };
 }
 
@@ -63,9 +63,9 @@ constexpr glm::vec3 as_rgb_norm(std::string_view hex_code)
   const auto color = as_rgb(hex_code);
 
   return {
-    color.x / 255.0f,
-    color.y / 255.0f,
-    color.z / 255.0f
+      color.x / 255.0f,
+      color.y / 255.0f,
+      color.z / 255.0f
   };
 }
 
@@ -75,10 +75,10 @@ constexpr glm::vec4 as_rgba_norm(std::string_view hex_code)
   const auto color = as_rgba(hex_code);
 
   return {
-    color.x / 255.0f,
-    color.y / 255.0f,
-    color.z / 255.0f,
-    color.w / 255.0f
+      color.x / 255.0f,
+      color.y / 255.0f,
+      color.z / 255.0f,
+      color.w / 255.0f
   };
 }
 
@@ -86,9 +86,9 @@ constexpr glm::vec4 as_rgba_norm(std::string_view hex_code)
 constexpr glm::vec3 as_rgb_norm(std::uint8_t r, std::uint8_t g, std::uint8_t b)
 {
   return {
-    r / 255.0f,
-    g / 255.0f,
-    b / 255.0f
+      r / 255.0f,
+      g / 255.0f,
+      b / 255.0f
   };
 }
 
@@ -96,10 +96,69 @@ constexpr glm::vec3 as_rgb_norm(std::uint8_t r, std::uint8_t g, std::uint8_t b)
 constexpr glm::vec4 as_rgba_norm(std::uint8_t r, std::uint8_t g, std::uint8_t b, std::uint8_t a)
 {
   return {
-    r / 255.0f,
-    g / 255.0f,
-    b / 255.0f,
-    a / 255.0f
+      r / 255.0f,
+      g / 255.0f,
+      b / 255.0f,
+      a / 255.0f
+  };
+}
+
+
+constexpr std::uint32_t as_rgb_uint(std::string_view hex_code)
+{
+  if (hex_code.size() < 7 || hex_code[0] != '#')
+    return 0xff << 24 | 0xff << 16 | 0xff << 8 | 0x0;
+
+  auto r = detail::as_decimal(hex_code[1]) << 4 | detail::as_decimal(hex_code[2]);
+  auto g = detail::as_decimal(hex_code[3]) << 4 | detail::as_decimal(hex_code[4]);
+  auto b = detail::as_decimal(hex_code[5]) << 4 | detail::as_decimal(hex_code[6]);
+
+  return {
+      static_cast<std::uint32_t>(r) << 24 |
+      static_cast<std::uint32_t>(g) << 16 |
+      static_cast<std::uint32_t>(b) << 8 |
+      0x0
+  };
+}
+
+
+constexpr std::uint32_t as_rgba_uint(std::string_view hex_code)
+{
+  if (hex_code.size() < 9 || hex_code[0] != '#')
+    return 0xff << 24 | 0xff << 16 | 0xff << 8 | 0xff;
+
+  auto r = detail::as_decimal(hex_code[1]) << 4 | detail::as_decimal(hex_code[2]);
+  auto g = detail::as_decimal(hex_code[3]) << 4 | detail::as_decimal(hex_code[4]);
+  auto b = detail::as_decimal(hex_code[5]) << 4 | detail::as_decimal(hex_code[6]);
+  auto a = detail::as_decimal(hex_code[7]) << 4 | detail::as_decimal(hex_code[8]);
+
+  return {
+      static_cast<std::uint32_t>(r) << 24 |
+      static_cast<std::uint32_t>(g) << 16 |
+      static_cast<std::uint32_t>(b) << 8 |
+      static_cast<std::uint32_t>(a)
+  };
+}
+
+
+constexpr std::uint32_t as_rgb_uint(std::uint8_t r, std::uint8_t g, std::uint8_t b)
+{
+  return {
+      static_cast<std::uint32_t>(r) << 24 |
+      static_cast<std::uint32_t>(g) << 16 |
+      static_cast<std::uint32_t>(b) << 8 |
+      0x0
+  };
+}
+
+
+constexpr std::uint32_t as_rgba_uint(std::uint8_t r, std::uint8_t g, std::uint8_t b, std::uint8_t a)
+{
+  return {
+      static_cast<std::uint32_t>(r) << 24 |
+      static_cast<std::uint32_t>(g) << 16 |
+      static_cast<std::uint32_t>(b) << 8 |
+      static_cast<std::uint32_t>(a)
   };
 }
 
